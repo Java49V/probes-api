bf814a0c06aed91f6adf4d752fc85deab0282bf0
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2024-02-03 22:19:07 +0300

    HW #81

--- src/main/java/telran/probes/dto/ProbeData.java
+++ src/main/java/telran/probes/dto/ProbeData.java
@@ -1,5 +1,24 @@
 package telran.probes.dto;
 
+import java.util.Objects;
+
 public record ProbeData(long sensorId, float value, long timestamp) {
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(sensorId, value);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProbeData other = (ProbeData) obj;
+		return sensorId == other.sensorId && Float.floatToIntBits(value) == Float.floatToIntBits(other.value);
+	}
 
 }
